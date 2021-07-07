547edc90351daf6600e3f680ba6de8bb92ea928b
Mishkin <9mihail@gmail.com>
2021-07-07 23:53:46 +0300

    added json file list countries and cities and configuration spinner menu for this list on new ad page

--- app/src/main/java/space/digitallab/noticeboard/act/EditAdsAct.kt
+++ app/src/main/java/space/digitallab/noticeboard/act/EditAdsAct.kt
@@ -1,8 +1,10 @@
 package space.digitallab.noticeboard.act
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import space.digitallab.noticeboard.databinding.ActivityEditAdsBinding
+import space.digitallab.noticeboard.utils.CitySearchHelper
 
 class EditAdsAct : AppCompatActivity() {
     private lateinit var rootElement: ActivityEditAdsBinding
@@ -10,5 +12,11 @@ class EditAdsAct : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         rootElement = ActivityEditAdsBinding.inflate(layoutInflater)
         setContentView(rootElement.root)
+
+        val adapter = ArrayAdapter(this,
+            android.R.layout.simple_spinner_item,
+            CitySearchHelper.getAllCountries(this))
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        rootElement.spCountry.adapter = adapter
     }
 }
