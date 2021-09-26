fceceabed5afecc733abae5108c6cdeee98080d8
Mishkin <9mihail@gmail.com>
2021-09-26 22:42:33 +0300

    #32 added Google Ads and BaseSelectImageFragment.kt & binding.apply ;)

--- app/src/main/java/space/digitallab/noticeboard/fragments/ImageListFragment.kt
+++ app/src/main/java/space/digitallab/noticeboard/fragments/ImageListFragment.kt
@@ -3,14 +3,11 @@ package space.digitallab.noticeboard.fragments
 import android.app.Activity
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.get
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.coroutines.CoroutineScope
@@ -19,34 +16,33 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import space.digitallab.noticeboard.R
 import space.digitallab.noticeboard.adapters.SelectImageRvAdapter
-import space.digitallab.noticeboard.databinding.ListImageFragmentBinding
 import space.digitallab.noticeboard.dialoghelper.ProgressDialog
 import space.digitallab.noticeboard.utils.AdapterCallBack
 import space.digitallab.noticeboard.utils.ImageManager
 import space.digitallab.noticeboard.utils.ImagePiker
 import space.digitallab.noticeboard.utils.ItemTouchMoveCallback
 
-class ImageListFragment(private val fragmentCloseInterface : FragmentCloseInterface, private val newList : ArrayList<String>?) : Fragment(), AdapterCallBack {
+class ImageListFragment(private val fragmentCloseInterface : FragmentCloseInterface, private val newList : ArrayList<String>?) : BaseSelectImageFragment(), AdapterCallBack {
 
-    lateinit var rootElement : ListImageFragmentBinding
+
     val adapter = SelectImageRvAdapter(this)
     val dragCallback = ItemTouchMoveCallback(adapter)
     val touchHealper = ItemTouchHelper(dragCallback)
     private var job: Job? = null
     private var addItem: MenuItem? = null
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        rootElement = ListImageFragmentBinding.inflate(inflater)
-        return rootElement.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setUpToolbar()
-        touchHealper.attachToRecyclerView(rootElement.rcViewSelectImage)
-        rootElement.rcViewSelectImage.layoutManager = LinearLayoutManager(activity)
-        rootElement.rcViewSelectImage.adapter = adapter
-        if(newList != null) resizeSelectedImage(newList, true)
+        binding.apply {
+            touchHealper.attachToRecyclerView(rcViewSelectImage)
+            rcViewSelectImage.layoutManager = LinearLayoutManager(activity)
+            rcViewSelectImage.adapter = adapter
+            if(newList != null) resizeSelectedImage(newList, true)
+        }
+
+
     }
 
     override fun onItemDelete() {
@@ -73,31 +69,38 @@ class ImageListFragment(private val fragmentCloseInterface : FragmentCloseInterf
         }
     }
 
-    private fun setUpToolbar(){
+    private fun setUpToolbar() {
 
-        rootElement.tb.inflateMenu(R.menu.menu_choose_image)
-        val deleteItem = rootElement.tb.menu.findItem(R.id.delete_image)
-        addItem = rootElement.tb.menu.findItem(R.id.add_image)
+        binding.apply {
 
-        rootElement.tb.setNavigationOnClickListener{
-            activity?.supportFragmentManager?.beginTransaction()?.remove(this)?.commit()
-        }
-        deleteItem.setOnMenuItemClickListener {
-            adapter.updateAdapter(ArrayList(), true)
-            addItem?.isVisible = true
-            true
-        }
-        addItem?.setOnMenuItemClickListener {
-            val imageCount = ImagePiker.MAX_IMAGE_COUNT - adapter.mainArray.size
-            ImagePiker.getImages(activity as AppCompatActivity, imageCount, ImagePiker.REQUEST_CODE_GET_IMAGES)
-            true
+            tb.inflateMenu(R.menu.menu_choose_image)
+            val deleteItem = tb.menu.findItem(R.id.delete_image)
+            addItem = tb.menu.findItem(R.id.add_image)
+
+            tb.setNavigationOnClickListener {
+                activity?.supportFragmentManager?.beginTransaction()?.remove(this@ImageListFragment)?.commit()
+            }
+            deleteItem.setOnMenuItemClickListener {
+                adapter.updateAdapter(ArrayList(), true)
+                addItem?.isVisible = true
+                true
+            }
+            addItem?.setOnMenuItemClickListener {
+                val imageCount = ImagePiker.MAX_IMAGE_COUNT - adapter.mainArray.size
+                ImagePiker.getImages(
+                    activity as AppCompatActivity,
+                    imageCount,
+                    ImagePiker.REQUEST_CODE_GET_IMAGES
+                )
+                true
+            }
         }
     }
 
     fun updateAdapter(newList : ArrayList<String>){ resizeSelectedImage(newList, false) }
 
     fun setSingleImage(uri : String, position : Int){
-        val pBar = rootElement.rcViewSelectImage[position].findViewById<ProgressBar>(R.id.pBar)
+        val pBar = binding.rcViewSelectImage[position].findViewById<ProgressBar>(R.id.pBar)
         job = CoroutineScope(Dispatchers.Main).launch {
             pBar.visibility = View.VISIBLE
             val bitmapList = ImageManager.imageResize(listOf(uri))
